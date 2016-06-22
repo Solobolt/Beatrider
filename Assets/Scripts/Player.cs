69e83630a203d10115f0657b1f243c92d547b714
Solobolt <solobolt@gmail.com>
2016-06-22 14:39:32 +1000

    Added new control scemes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,19 +2,34 @@
 using UnityEngine.UI;
 using System.Collections;
 
+public enum Control
+{
+    PianoKeys,
+    SixKeys
+}
+
 public class Player : MonoBehaviour {
 
     private RectTransform myTransform;
 
-    public float moveSpeed = 0.5f;
+    public float moveSpeed = 0.1f;
+    public Control controlType = Control.PianoKeys;
 
+    
     private Vector3 targetPos = new Vector3(0, 0, 0);
     private Vector3 lastTarget = new Vector3(0, 0, 0);
     private float distnace;
-    private float moveTime = 0.1f;
+    private float moveTime = 0.5f;
     private float direction = 1f;
 
-    #region Stuff
+    #region SixKeysValues
+    private float majorNumDelta = 200.0f;
+    private float minorNumDelta = 50.0f;
+    private float majorNum = 0.0f;
+    private float minorNum = 0.0f;
+    #endregion
+
+    #region LocationValues
     private Vector3 posL3 = new Vector3(-250, 0, 0);
     private Vector3 posL2 = new Vector3(-200, 0, 0);
     private Vector3 posL1 = new Vector3(-150, 0, 0);
@@ -33,13 +48,68 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Controls();
-        MovePlayer();
+        switch(controlType)
+        {
+            case Control.PianoKeys:
+                PianoKeys();
+                MovePlayer();
+                break;
+
+            case Control.SixKeys:
+                SixKeys();
+                MovePlayer();
+                break;
+
+            default:
+                PianoKeys();
+                MovePlayer();
+                break;
+        }
 	}
 
+    //Handles 6 sey system
+    void SixKeys()
+    {
+        
+        //Handles major Movement
+        if(Input.GetKeyDown("a"))
+        {
+            majorNum = (-majorNumDelta);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            majorNum = 0.0f;
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            majorNum = majorNumDelta;
+        }
+
+        //Handles minor movement
+        if (Input.GetKeyDown("j"))
+        {
+            minorNum = (-minorNumDelta);
+        }
+
+        if (Input.GetKeyDown("k"))
+        {
+            minorNum = 0.0f;
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            minorNum = minorNumDelta;
+        }
+
+
+        targetPos = new Vector3(majorNum + minorNum,0,0);
+
+    }
 
     //Handles Piano Style Controls
-    void Controls()
+    void PianoKeys()
     {
         if(Input.GetKeyDown("a"))
         {
