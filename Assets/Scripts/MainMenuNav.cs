08dc07bfb1eac25ddc968b5121fdbdc069681388
Solobolt <solobolt@gmail.com>
2016-07-05 15:00:05 +1000

    Started adding Pop Goes the weazel

--- Assets/Scripts/MainMenuNav.cs
+++ Assets/Scripts/MainMenuNav.cs
@@ -4,10 +4,25 @@ using System.Collections;
 
 public class MainMenuNav : MonoBehaviour {
 
+    //Holds 'Are you sure' screen
+    public GameObject areYouSureScreen;
+
     //starts the game
     public void PlayGame(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    //Show are you sure screen
+    public void ShowSureScreen()
+    {
+        areYouSureScreen.SetActive(true);
+    }
+
+    //Hide are you sure screen
+    public void HideSureScreen()
+    {
+        areYouSureScreen.SetActive(false);
     }
 
     //Exits game
