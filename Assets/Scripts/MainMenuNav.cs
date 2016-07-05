eb1e77703d13e0d356e9032cee42b3a3ecf36f5c
Solobolt <solobolt@gmail.com>
2016-07-05 14:16:43 +1000

    Added Main Menu SCript and navigation

---
+++ Assets/Scripts/MainMenuNav.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuNav : MonoBehaviour {
+
+    //starts the game
+    public void PlayGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    //Exits game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
