aa945ae250cc618d67c01ad2d9789428b403d1c4
Therratos <1001983@student.sae.edu.au>
2016-07-08 18:36:22 +1000

    pushing changes to background colour script
    makes a nice transitional lerp now

--- Assets/Scripts/BackgroundColor.cs
+++ Assets/Scripts/BackgroundColor.cs
@@ -2,6 +2,8 @@
 using UnityEngine.UI;
 using System.Collections;
 
+
+
 //Background colours
 public enum BackColor
 {
@@ -13,44 +15,48 @@ public enum BackColor
 
 public class BackgroundColor : MonoBehaviour {
 
+	 Color temp;
+
     public Image background;
+
+	public bool happyTrue;
+	public bool calmTrue;
+	public bool sadTrue;
+	public bool anxiousTrue;
+
 
     public void changeColour(BackColor BC)
     {
         switch(BC)
         {
-            case BackColor.Calm:
-                background.color = new Vector4 
-                    (
-                    131.0f/255.0f,
-                    197f/255.0f,
-                    210f/255.0f,
-                    255/255.0f
-                    );
+		case BackColor.Calm:
+			temp  = new Vector4 (131.0f / 255.0f,
+				197f / 255.0f,
+				210f / 255.0f,
+				255 / 255.0f);
                 break;
 
             case BackColor.Anxious:
-                background.color = new Vector4
-                    (
-                    39f / 255.0f,
-                    5f / 255.0f,
-                    5f / 255.0f,
-                    255 / 255.0f
-                    );
+
+			temp = new Vector4      (
+				39f / 255.0f,
+				5f / 255.0f,
+				5f / 255.0f,
+				255 / 255.0f
+			);	
                 break;
 
-            case BackColor.Depressed:
-                background.color = new Vector4
-                    (
-                    10f / 255.0f,
-                    10f / 255.0f,
-                    50f / 255.0f,
-                    255 / 255.0f
-                    );
+		case BackColor.Depressed:
+			temp = new Vector4 (
+				10f / 255.0f,
+				10f / 255.0f,
+				50f / 255.0f,
+				255 / 255.0f
+			);
                 break;
 
             case BackColor.Happy:
-                background.color = new Vector4
+			temp = new Vector4
                     (
                     145f / 255.0f,
                     184f / 255.0f,
@@ -60,7 +66,7 @@ public class BackgroundColor : MonoBehaviour {
                 break;
 
             default:
-                background.color = new Vector4(0, 0, 0, 1);
+			background.color = new Vector4(0, 0, 0, 0.1f);
                 break;
         }
     }
@@ -73,5 +79,17 @@ public class BackgroundColor : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if (background.color != temp) { 
+			background.color = Vector4.Lerp (background.color, temp, 0.1f);
+		}
+
+
+
+
+
+
+
+
+
 	}
 }
